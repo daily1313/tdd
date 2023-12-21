43b97fc5fa07fffcb4412f1163b9d0a53de34857
김승범 <kimsb7218@naver.com>
2023-12-21 15:51:13 +0900

    refactor: Remove Unused imports

--- src/test/java/com/example/tdd/ApiTest.java
+++ src/test/java/com/example/tdd/ApiTest.java
@@ -2,7 +2,6 @@ package com.example.tdd;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
